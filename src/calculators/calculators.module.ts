0ec55f1d57abc9cf95b68686b7a3c119bee34db9
ffekirnew <ffekirnew0808@gmail.com>
2023-01-28 16:44:14 +0300

    deleted unwanted module

--- src/calculators/calculators.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CalculatorsService } from './calculators.service';
-import { CalculatorsController } from './calculators.controller';
-
-@Module({
-  controllers: [CalculatorsController],
-  providers: [CalculatorsService]
-})
-export class CalculatorsModule {}
